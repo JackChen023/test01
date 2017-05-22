c62e4808353b12eb30e4bf62f9ba7e4ce3964d2f
heyudongfang <heyudongfang@heyudongfangdeiMac.local>
2017-05-22 14:19:19 +0800

    xxx

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by heyudongfang on 2017/5/22.
+//  Copyright © 2017年 CHENMENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by heyudongfang on 2017/5/22.
+//  Copyright © 2017年 CHENMENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
